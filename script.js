6b4c428c696838c8cb78281d77627f3e0ee7f428
Nick White <bbycinka@gmail.com>
2022-05-02 17:30:41 +0300

    первая версия to-do

---
+++ script.js
@@ -0,0 +1,35 @@
+const initialTasks = ["твоя задача"];
+
+const template = document.querySelector("#template").content;
+const todoList = document.querySelector(".todo__list");
+const todoName = document.querySelector(".todo__name");
+const todoForm = document.querySelector(".todo__form");
+
+todoForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  if (todoName.value === "") {
+    addTask("задача отсутствует:)");
+    todoName.value = "";
+  } else {
+    addTask(todoName.value);
+    todoName.value = "";
+  }
+});
+
+function task(text) {
+  const task = template.cloneNode(true);
+  task.querySelector(".todo__text").textContent = text;
+  task.querySelector(".todo__button").addEventListener("click", function (e) {
+    e.preventDefault();
+    e.target.closest(".todo__item").remove();
+  });
+  return task;
+}
+
+function addTask(text) {
+  todoList.prepend(task(text));
+}
+
+initialTasks.forEach(function (i) {
+  todoList.prepend(task(i));
+});
